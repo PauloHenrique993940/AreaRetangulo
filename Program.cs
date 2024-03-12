b4c9b6a0968d6d8ee29727eb7c16f3d373c63306
Paulo Henrique <paulohenriqueferreirafranca2@gmail.com>
2024-03-12 10:19:29 -0300

    Programa resolvido no modo estrutural

--- Program.cs
+++ Program.cs
@@ -6,7 +6,32 @@ namespace AreaRetangulo
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //Programa Estrutural 
+            // Obtendo dados da largura 
+            Console.WriteLine("Digite a largura do Retângulo: ");
+            double largura = double.Parse(Console.ReadLine());
+
+            // Obtendo dados da altura
+            Console.WriteLine("Digite a altura do Retângulo: ");
+            double altura = double.Parse(Console.ReadLine());
+
+            // Calculando área do Retângulo
+            double area = largura * altura;
+
+            // Calculando o perímetro
+            double perimetro = 2 * (largura + altura);
+
+            // Calculando a diagonal do retangulo
+            double diagonal = Math.Sqrt((largura * largura) + (altura * altura));
+
+            // Exibindo os resultados
+            Console.WriteLine("A área do retângulo é: " + area);
+            Console.WriteLine("O perímetro do retângulo é: " + perimetro);
+            Console.WriteLine("A diagonal do retângulo é: " + diagonal);
+
+            
+            Console.ReadLine();
+
         }
     }
 }
