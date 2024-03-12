f6fcd851fcc577a874b2ce6bcae75b9a6cddf5a5
Paulo Henrique <paulohenriqueferreirafranca2@gmail.com>
2024-03-12 09:52:55 -0300

    Criando Projetp Area do Retangulo

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AreaRetangulo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
